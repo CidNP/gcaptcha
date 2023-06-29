10e6b5d5ca03cd5727dd525c225b78834bd24bc0
Abhishek Byanjankar <2011009_abhishek@kusom.edu.np>
2023-06-29 19:46:48 +0900

    js working?

--- main.js
+++ main.js
@@ -9,6 +9,16 @@ const submitButton = document.querySelector(".button");
 //variable to store generated captcha
 let captchaText = null;
 
+//Function to generate captcha
 
+const generateCaptcha = () => {
+    const randomString = Math.random().toString(36).substring(2, 7);
+    const randomStringArray = randomString.split("");
+    const changeString = randomStringArray.map((char) => 
+    (Math.random() > 0.5 ? char.toUpperCase() : char));
+    captchaText = changeString.join("  ");
+    captchaTextBox.value= captchaText;
+    console.log(captchaText);
+};
 
 

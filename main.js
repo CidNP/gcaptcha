2715a804c94f6d9ff37044c490c1fdb4a5a6cbb6
Abhishek Byanjankar <2011009_abhishek@kusom.edu.np>
2023-06-29 19:42:36 +0900

    js here we go

---
+++ main.js
@@ -0,0 +1,14 @@
+// selecting DOM elements
+
+const captchaTextBox = document.querySelector(".captcha-box input");
+const resetButton = document.querySelector(".reset-button");
+const captchaInputBox = document.querySelector(".captcha-input input");
+const message = document.querySelector(".message");
+const submitButton = document.querySelector(".button");
+
+//variable to store generated captcha
+let captchaText = null;
+
+
+
+
